22f108ce215522e8314ca62f81efa69f0a9abae7
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2025-05-23 15:37:35 +0400

    Ignore errors on cache cleaning

--- crates/command/clean/src/lib.rs
+++ crates/command/clean/src/lib.rs
@@ -7,6 +7,6 @@ pub struct FeatureCleanOptions;
 pub fn clean(_opts: FeatureCleanOptions) -> Result<()> {
     let ctx = load_invocation_context()?;
     let cache_dir = ctx.cache_dir;
-    std::fs::remove_dir_all(cache_dir)?;
+    let _ = std::fs::remove_dir_all(cache_dir);
     Ok(())
 }
