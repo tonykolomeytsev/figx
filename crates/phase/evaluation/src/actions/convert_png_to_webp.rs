0345a28b07db2708eaf1c6e64c4cdbab111dfc84
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2025-05-23 15:52:50 +0400

    Add transform caching

--- crates/phase/evaluation/src/actions/convert_png_to_webp.rs
+++ crates/phase/evaluation/src/actions/convert_png_to_webp.rs
@@ -1,9 +1,25 @@
 use crate::{Error, EvalContext, Result};
+use lib_cache::CacheKey;
 use log::debug;
 
-pub fn convert_png_to_webp(_ctx: &EvalContext, args: ConvertPngToWebpArgs) -> Result<Vec<u8>> {
+const WEBP_TRANSFORM_TAG: u8 = 0x02;
+
+pub fn convert_png_to_webp(ctx: &EvalContext, args: ConvertPngToWebpArgs) -> Result<Vec<u8>> {
+    // construct unique cache key
+    let cache_key = CacheKey::builder()
+        .set_tag(WEBP_TRANSFORM_TAG)
+        .write(args.bytes)
+        .write_str(&args.quality.to_string())
+        .build();
+
     debug!("transforming: png to webp (q={})", args.quality);
 
+    // return cached value if it exists
+    if let Some(webp) = ctx.cache.get_bytes(&cache_key)? {
+        return Ok(webp);
+    }
+
+    // otherwise, do transform
     let png = image::load_from_memory_with_format(&args.bytes, image::ImageFormat::Png)?;
     let encoder = webp::Encoder::from_image(&png).map_err(|_| Error::WebpCreate)?; // fails if img is not RBG8 or RBGA8
     let webp = if args.quality == 100.0 {
@@ -11,6 +27,9 @@ pub fn convert_png_to_webp(_ctx: &EvalContext, args: ConvertPngToWebpArgs) -> Re
     } else {
         encoder.encode(args.quality)
     };
+
+    // remember result to cache
+    ctx.cache.put_slice(&cache_key, &webp)?;
     Ok(webp.to_vec())
 }
 
